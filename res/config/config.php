4f56130a4f4354879df4089054df36383824c420
netrix1 <46089134+netrix1@users.noreply.github.com>
2022-12-01 01:52:32 -0300

    itens que depois Irão para o banco

--- res/config/config.php
+++ res/config/config.php
@@ -3,8 +3,8 @@
 session_start();
 require_once("vendor/autoload.php");
 
-define("ISYS_TITLE","iSys");
-define("ISYS_CHURCH_NAME","Assembléia de Deus Min. Deus é Luz");
+define("ISYS_TITLE","iSys"); // future data in DB
+define("ISYS_CHURCH_NAME","Assembléia de Deus Min. Deus é Luz"); // future data in DB
 
 define("ISYS_TYPE_MENU_PAGE","1");
 define("ISYS_TYPE_PAGE","2");
